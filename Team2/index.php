1e2c63ed01c886cfd42b107d5d1ed0728642747e
lore <lore@10.43.71.2>
2017-10-31 16:36:52 +0100

    First design caregiver.

---
+++ Team2/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>
+            Test page made with a framework
+        </title>
+        <!-- Latest compiled and minified bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+        <link rel="stylesheet/less" type="text/css" href="styles/main.less" />
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js">
+        </script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>
+                        Graceage: globale overzicht
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <div>     
+                        <button class="btn btn-default btn-lg btn-block">
+                            <a href="#"> algemeen </a>
+                        </button> 
+                    </div>
+                    </br>
+                    <div class="dropdown">
+                            <button data-toggle="dropdown" class="btn btn-default dropdown-toggle btn-lg btn-block">
+                                Verdieping
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#"> Gelijkvloers </a>
+                                </li>
+                                <li>
+                                    <a href="#"> Eerste verdiep </a>
+                                </li>
+                                <li>
+                                    <a href="#"> Tweede verdiep</a>
+                                </li>
+                                <li>
+                                    <a href="#"> Derde verdiep</a>
+                                </li>
+                            </ul>
+                    </div>
+                    </br>
+                    <div>
+                        <button type="button" class="btn btn-default btn-lg btn-block">
+                            <a href="#">Bewoners</a>
+                        </button> 
+                    </div>
+                    </br>
+                </div>
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h3>
+                                Vragenlijst resultaten
+                            </h3>
+                            <div class="panel-group" id="panel-246282">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                         <a class="panel-title" data-toggle="collapse" data-parent="#panel-246282" href="#panel-element-961573"> Bewoners overzicht </a>
+                                    </div>
+                                    <div id="panel-element-961573" class="panel-collapse collapse in">
+                                        <div class="panel-body">
+                                           <a href=#> Marie </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Jef </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Hans </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Lieven </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Maria </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Jozef </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Rik </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Marie </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Jean </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Marie </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                         <a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-246282" href="#panel-element-992438"> Vragen overzicht </a>
+                                    </div>
+                                    <div id="panel-element-992438" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            vraag 1
+                                            <img class="img-rounded img-respnsive" src=result.jpg  alt="resultaat">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h3>
+                                Overzicht invultijd
+                            </h3>
+                            <div class="panel-group" id="panel-527694">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                         <a class="panel-title" data-toggle="collapse" data-parent="#panel-527694" href="#panel-element-889044">Bewoners overzicht</a>
+                                    </div>
+                                    <div id="panel-element-961573" class="panel-collapse collapse in">
+                                        <div class="panel-body">
+                                           <a href=#> Korneel </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Anabel </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Joske </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Lore </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Steven </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Sven </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Hendrik </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Kobe </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Senne </a>
+                                        </div>
+                                        <div class="panel-body">
+                                           <a href=#> Emile </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        <center>HCI/webapps project: team 2</center>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
