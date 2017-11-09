125fe266fa7fe247df635204b9e3e67769bbd236
ruben.pauly1 <trex.ruben@hotmail.com>
2017-11-09 15:21:46 +0100

    Login page for elderly and to test comit

---
+++ Team2/application/views/pages_residents/loginR.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>
+            Test page made with a framework
+        </title>
+        <!-- Latest compiled and minified bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+        <link rel="stylesheet/less" type="text/css" href="../styles/main-loginR.less" />
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>
+                        Graceage: login bewoners
+                    </h1>
+                
+                    <h2>
+                        Welkom
+                    </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div>     
+                    <button class="btn btn-default btn-lg ">
+                        vrouw
+                    </button>
+                    <button type="button" class="btn btn-default btn-lg">
+                        Man
+                    </button> 
+                </div>
+            </div>
+            <div class="row">
+                <h2>
+                    Ik ben:
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+              </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#">
+                        <img src="#" class="img-rounded img-respnsive" alt="example">
+                        <p> Maria </p>
+                    </a>
+                </div>
+              </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        <center>HCI/webapps project: team 2</center>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
