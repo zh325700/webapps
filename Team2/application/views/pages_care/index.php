11bb7da35ff38dedb30c7f97574f656ebedb2916
Shuaigehan <zh325700@gmail.com>
2017-11-21 17:33:16 +0100

    overview strach , have a nother idea of searching residents

--- Team2/application/views/pages_care/index.php
+++ Team2/application/views/pages_care/index.php
@@ -3,12 +3,11 @@
 <?php $cout = 0; ?>
 <?php foreach ($residents as $res): ?>
 
-    <div style="display: inline-block" >
-        <a href="<?php echo site_url('index.php/Residents_control/' . $res['ID_Elder']); ?>">
-            <img class="ImageBorder"  src="<?= base_url() ?>images/icons/<?php echo $res['Picture']; ?>" alt="<?php echo $res['LastName'] ?>" style="width:500px;height:500px;">
-             <figcaption>Caption goes here</figcaption>
+    <div style="display: inline-block">
+        <a class="Imagelayout" href="<?php echo site_url('index.php/Residents_control/' . $res['ID_Elder']); ?>">
+            <img src="<?= base_url() ?>images/icons/<?php echo $res['Picture']; ?>" alt="<?php echo $res['LastName'] ?>" style="width:360px;height:360px;border:10px blue;">
+             <figcaption class="text-center"><?php echo $res['LastName']; ?></figcaption>
         </a>
     </div>
-
 
 <?php endforeach; ?>
--- Team2/application/views/templates_residents/header.php
+++ Team2/application/views/templates_residents/header.php
@@ -1,9 +1,9 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>ciBlog</title>     
+        <title>Remy House</title>     
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">       
-        <link href="../assets/css/overview-Res.css" rel="stylesheet" type="text/css"/>
+        <link href="<?= base_url() ?>assets/css/overview-Res.css" rel="stylesheet" type="text/css"/>
         <script src="https://cdn.ckeditor.com/4.7.3/standard/ckeditor.js"></script>
     </head>
 <body>
