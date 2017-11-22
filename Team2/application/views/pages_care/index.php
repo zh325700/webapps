f2128a8f72247dd66e305a350e381ae4cc171373
Shuaigehan <zh325700@gmail.com>
2017-11-22 22:16:02 +0100

    Second version of overview, can find all people with same name

--- Team2/application/views/pages_care/index.php
+++ Team2/application/views/pages_care/index.php
@@ -27,23 +27,30 @@
 
 <?php endforeach; ?>
 <div>
-<form action='' method='post'>
-    <p><label>LastName:</label><input type='text' name='LastName' value='' class='auto'></p>
-</form>
+    <form action='' method='post'>
+        <p><label>Find By LastName:</label><input type='text' name='LastName' value='' class='auto'></p>
+    </form>
 </div>
-<?php if (isset($_POST["LastName"])): $lastname = $_POST["LastName"]; ?>
-<?php if(array_search($lastname, array_column($array, 'LastName'))): ?>
-<?php echo $lastname ?>
-<?php $found_key = array_search($lastname, array_column($array, 'LastName'));
-$pic = $array[$found_key]['Picture'];
-$id = $array[$found_key]['ID_Elder'];
-?>
-<div style="display: inline-block">
-    <a class="Imagelayout" href="<?php echo site_url('index.php/Residents_control/' . $id); ?>">
-        <img src="<?= base_url() ?>images/icons/<?php echo $pic; ?>" alt="<?php echo $lastname ?>" style="width:360px;height:360px;border:10px blue;">
-        <figcaption class="text-center"><?php echo $lastname; ?></figcaption>
-    </a>
-</div>
-<?php endif; ?>
+<?php if (isset($_POST["LastName"])): $lastname = $_POST["LastName"]; ?>      <!--if you input something in the text field-->
+    <?php
+    $filteredArray = array_filter($array, function($element) use($lastname) {
+        return isset($element['LastName']) && $element['LastName'] == $lastname;
+    });
+    ?><!--find all arrays that have the same lastname and store in $filteredArray-->
+
+    <?php foreach ($filteredArray as $fArray): ?>
+        <?php
+        $pic = $fArray['Picture'];
+        $id = $fArray['ID_Elder'];
+        ?>
+        <div style="display: inline-block">
+            <a class="Imagelayout" href="<?php echo site_url('index.php/Residents_control/' . $id); ?>">
+                <img src="<?= base_url() ?>images/icons/<?php echo $pic; ?>" alt="<?php echo $lastname ?>" style="width:360px;height:360px;border:10px blue;">
+                <figcaption class="text-center"><?php echo $lastname; ?></figcaption>
+            </a>
+        </div>
+    <?php endforeach; ?>
+
+
 <?php endif; ?>
 
