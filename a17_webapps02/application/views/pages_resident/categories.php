0e2a4676ae406b4742a51c898f49820c768bda4d
Lore <lore.donne@student.kuleuven.be>
2017-12-07 22:06:24 +0100

    Fixed styling category page.

--- a17_webapps02/application/views/pages_resident/categories.php
+++ a17_webapps02/application/views/pages_resident/categories.php
@@ -1,77 +1,74 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
-			<h1 class="text-center bar1"></h1>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4"></div>
+                <div class="col-md-4"></div>
 		<div class="col-md-4">
-			<h2 class="text-center bar2" >Please chose a category</h2>                   
+			<h2 class="text-center bar2" >Please chose a category</h2>
 		</div>
-		<div class="col-md-4">
-			<img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" style=" width: 150px" class="align-right"/>
-		</div>
+                <div class="col-md-4">
+                    <img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" class="align-right"/>
+                </div>
 	</div>
+
 	<div class="row">
-		<div class="col-md-12"></div>
-	</div>
-	<div class="row">
-		<div class="col-md-3" style="top:-100px; left: 15px">
-				<button id = "newButton"  type="button" class="btn btn-lg active category" onclick="loadPage('Question','getQuestion?category=Privacy')">
+                <div class="col-md-2"></div>
+		<div class="col-md-2">
+				<button id = "newButton"  type="button" class="btn btn-lg active style2 category1" onclick="loadPage('Question','getQuestion?category=Privacy')">
 					Privacy
 				</button> 
 		</div>
-		<div class="col-md-3" style="top:-100px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-top: 0px; padding-bottom: 0px" onclick="loadPage('Question','getQuestion?category=FoodAndMeals')">
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category6" onclick="loadPage('Question','getQuestion?category=FoodAndMeals')">
                         Food <br/> and <br/> Meals
                     </button>                     
 		</div>
-		<div class="col-md-3" style="top:-100px; right: 15px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" onclick="loadPage('Question','getQuestion?category=Comfort')">
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category2" onclick="loadPage('Question','getQuestion?category=Comfort')">
                         Comfort
                     </button>                     
 		</div>
-		<div class="col-md-3" style="top:-100px; right: 35px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" onclick="loadPage('Question','getQuestion?category=Activities')">
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category2" onclick="loadPage('Question','getQuestion?category=Activities')">
                         Activities
                     </button> 
-		</div>            
+		</div>   
+                <div class="col-md-2"></div>
 	</div>
 	<div class="row">
-		<div class="col-md-3" style="top:-80px; left: 15px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-top: 25px; padding-bottom: 25px" onclick="loadPage('Question','getQuestion?category=PersonalRelationships')">
+                <div class="col-md-2"></div>
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category3" onclick="loadPage('Question','getQuestion?category=PersonalRelationships')">
                         Personal <br/> relations
                     </button> 
 		</div>
-		<div class="col-md-3" style="top:-80px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-top: 25px; padding-bottom: 25px" onclick="loadPage('Question','getQuestion?category=DailyDecisions')">
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category3" onclick="loadPage('Question','getQuestion?category=DailyDecisions')">
                         Daily <br/> decisions
                     </button>                     
 		</div>
-		<div class="col-md-3"style="top:-80px; right: 15px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-top: 0px; padding-bottom: 0px" onclick="loadPage('Question','getQuestion?category=RespectByStaff')">
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category" onclick="loadPage('Question','getQuestion?category=RespectByStaff')">
                         Respect <br/> by <br/> staff
                     </button>                     
 		</div>
-		<div class="col-md-3" style="top:-80px; right: 35px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-top: 0px; padding-bottom: 0px" onclick="loadPage('Question','getQuestion?category=StaffResidentBonding')">
+		<div class="col-md-2">
+                    <button id = "newButton"  type="button" class="btn btn-lg active style2 category" onclick="loadPage('Question','getQuestion?category=StaffResidentBonding')">
                         Staff <br/> resident <br/> bonding
                     </button> 
-		</div>            
+		</div>       
+                <div class="col-md-2"></div>
 	</div>
 	<div class="row">
-		<div class="col-md-3" style="bottom:60px"></div>
-		<div class="col-md-3" style="bottom:60px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-bottom: 0px; padding-top: 0px; padding-left: 10px; width: 205%; left: -215px" onclick="loadPage('Question','getQuestion?category=StaffResponsiveness')">
+		
+		<div class="col-md-6">
+                    <button id = "newButton"  type="button" class="btn btn-lg active category4" onclick="loadPage('Question','getQuestion?category=StaffResponsiveness')">
                         Staff <br/> responsiveness
                     </button>                     
 		</div>
-		<div class="col-md-3" style="bottom:60px">
-                    <button id = "newButton"  type="button" class="btn btn-lg active category" style="padding-bottom: 0px; padding-top: 0px; width: 205%; right: 15px" onclick="loadPage('Question','getQuestion?category=SafetyAndSecurity')">
+		<div class="col-md-6">
+                    <button id = "newButton"  type="button" class="btn btn-lg active category5" onclick="loadPage('Question','getQuestion?category=SafetyAndSecurity')">
                         Safety and <br/> Security
                     </button>                     
 		</div>
-		<div class="col-md-3" style="bottom:60px"></div>            
+		           
 	</div>
 </div>
--- a17_webapps02/application/views/pages_resident/question.php
+++ a17_webapps02/application/views/pages_resident/question.php
@@ -13,13 +13,13 @@
 	</div>
 	<div class="row">
 		<div class="col-md-4">
-			<img src="<?php echo base_url(); ?>/image/pictograms/food.png" alt="" style=" width: 150px" class="align-left"/>
+			<img src="<?php echo base_url(); ?>/image/pictograms/food.png" alt="" class="align-left"/>
 		</div>
 		<div class="col-md-4">
 			<h2 class="text-center bar2" id = "question_number">Question 1</h2>
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" style=" width: 150px" class="align-right"/>			
+			<img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" class="align-right"/>			
 		</div>
 	</div>
 	<div class="row">
@@ -29,7 +29,7 @@
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<img src="<?php echo base_url(); ?>/image/pictograms/Smileys.png" alt="" style="width:850px" class="center" usemap="#map"/>
+			<img src="<?php echo base_url(); ?>/image/pictograms/Smileys.png" alt="" class="center" usemap="#map"/>
 			<map name="map">
 					<area shape="circle" coords="80,90,80" href="javascript:getQuestion(1)" id="smiley" >
 					<area shape="circle" coords="250,90,80" href="javascript:getQuestion(2)" id="smiley">
