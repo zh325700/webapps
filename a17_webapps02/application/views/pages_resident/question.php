4dfd87d00adbcad434e2fc4819df7e3dedb796e5
Lore <lore.donne@student.kuleuven.be>
2017-12-07 22:22:26 +0100

    Merge

--- a17_webapps02/application/views/pages_resident/question.php
+++ a17_webapps02/application/views/pages_resident/question.php
@@ -6,20 +6,16 @@
 
 
 <div class="container-fluid">
-    <div class="row">
-		<div class="col-md-12">
-			<h1 class="text-center bar1"></h1>
-		</div>
-	</div>
+
 	<div class="row">
 		<div class="col-md-4">
-			<img src="<?php echo base_url(); ?>/image/pictograms/food.png" alt="" class="align-left"/>
+			<img src="<?php echo base_url(); ?>/image/pictograms/food.png" alt="" style=" width: 150px" class="align-left"/>
 		</div>
 		<div class="col-md-4">
 			<h2 class="text-center bar2" id = "question_number">Question 1</h2>
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" class="align-right"/>			
+			<img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" style=" width: 150px" class="align-right"/>			
 		</div>
 	</div>
 	<div class="row">
@@ -29,7 +25,7 @@
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<img src="<?php echo base_url(); ?>/image/pictograms/Smileys.png" alt="" class="center" usemap="#map"/>
+			<img src="<?php echo base_url(); ?>/image/pictograms/Smileys.png" alt="" style="width:850px" class="center" usemap="#map"/>
 			<map name="map">
 					<area shape="circle" coords="80,90,80" href="javascript:getQuestion(1)" id="smiley" >
 					<area shape="circle" coords="250,90,80" href="javascript:getQuestion(2)" id="smiley">
