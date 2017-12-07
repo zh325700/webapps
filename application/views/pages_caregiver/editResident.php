3ce6abb0f460fb63afad1f6b4bc9d9e27fe9e74c
Yehualashet <Yehualashet@10.92.176.108>
2017-12-07 19:26:32 +0100

    styled

--- application/views/pages_caregiver/editResident.php
+++ application/views/pages_caregiver/editResident.php
@@ -1,11 +1,12 @@
 <div class="container-fluid flex">
 
     <div class="row justify-content-md-center">
-        <div class="col-md-6">
+        <div class="col col-md-2"></div>
+          <div class="col-md-6">
             <h2 class=" text-center headertwo">
                 Edit Residents
             </h2>
-        </div>
+          </div>
     </div>
 
     <?php echo validation_errors(); ?>
@@ -16,102 +17,95 @@
             <input type="hidden" name="ID_Elder" value="<?php echo $resident['ID_Elder']; ?>" >
             <div class="row insert-row">
                 <div class="col col-md-1"></div>
-                <div class="col col-md-4 fontsize">
-                    First Name
+                <div class="col col-md-4">
+                    <label>First Name: </label> 
                 </div>
-                <div class="col-md-5">
-                    <div class="form-group insert-form">
-                        <input type="text" class="form-control" name="FirstName" placeholder="Add FirstName"
-                               value="<?php echo $resident['FirstName']; ?>">
+                
+                    <div class="col-md-5 input-group">
+                        <input type="text" class="form-control" name="FirstName" placeholder="Add FirstName" value="<?php echo $resident['FirstName']; ?>">
                     </div>
-                </div>
             </div>
             <div class="row insert-row">
                 <div class="col col-md-1"></div>
                 <div class="col col-md-4 fontsize">
-                    Last Name
+                    <label>Last Name:</label> 
                 </div>
-                <div class="col-md-5">
-                    <div class="form-group insert-form">
-                        <input type="text" class="form-control" name="LastName" placeholder="Add LastName"
-                               value="<?php echo $resident['LastName']; ?>">
+                
+                     <div class="col-md-5 input-group">
+                        <input type="text" class="form-control" name="LastName" placeholder="Add LastName" value="<?php echo $resident['LastName']; ?>">
                     </div>
-                </div>
+                
             </div>
 
             <div class="row insert-row">
                 <div class="col col-md-1"></div>
-                <div class="col col-md-4 fontsize">
-                    Gender
+                <div class="col col-md-4">
+                     <label>Gender:</label> 
                 </div>
-                <div class="col-md-5" id="checkboxGroup">
+                <div class="col-md-6" id="checkboxGroup">
                     <div class="form-group">
-                        <label>
+                        
                             <div class=" col-md-3">
                                 <img src="https://cdn2.iconfinder.com/data/icons/person-gender-hairstyle-clothes-variations/48/Female-Side-comb-O-neck-512.png" style="width:40px;height:40px;">
 
-                                <input type="checkbox"name="Sex" value="F" style="width:15px;height:15px;text-align: center;"></div>
-                        </label>
-                        <label>
+                                <input type="checkbox"name="Sex" value="F" style="width:15px;height:15px;text-align: center;">
+                            </div>
+                        
                             <div class=" col-md-3">
                                 <img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/256/user-male-icon.png" style="width:40px;height:40px;">
 
                                 <input type="checkbox"name="Sex" value="M" style="width:15px;height:15px;text-align: center;">
                             </div>
-                        </label>
                     </div>
+                       
                 </div>
+            </div>
+        
+
+            <div class="row insert-row">
+                <div class="col col-md-1"></div>
+                <div class="col col-md-4 fontsize">
+                    <label>Birthday:</label>
+                </div>
+                
+                    <div class="col-md-5 input-group">
+                        <input type="text" class="form-control" name="Birthday" placeholder="Add Birthday" value="<?php echo $resident['Birthday']; ?>">
+                    </div>
+                
             </div>
 
             <div class="row insert-row">
                 <div class="col col-md-1"></div>
                 <div class="col col-md-4 fontsize">
-                    Birthday
+                    <label>Room Number:</label>
                 </div>
-                <div class="col-md-5">
-                    <div class="form-group insert-form">
-                        <input type="date" class="form-control" name="Birthday" placeholder="Add Birthday"
-                               value="<?php echo $resident['Birthday']; ?>">
+                
+                    <div class="col-md-2 input-group">
+                        <input type="text" class="form-control" name="RoomNumber" placeholder="Add Room number" value="<?php echo $resident['RoomNumber']; ?>">
                     </div>
-                </div>
+                
             </div>
 
             <div class="row insert-row">
                 <div class="col col-md-1"></div>
                 <div class="col col-md-4 fontsize">
-                    Room Number
+                    <label>Facility:</label>
                 </div>
-                <div class="col-md-5">
-                    <div class="form-group insert-form">
-                        <input type="text" class="form-control" name="RoomNumber" placeholder="Add Room number"
-                               value="<?php echo $resident['RoomNumber']; ?>">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row insert-row">
-                <div class="col col-md-1"></div>
-                <div class="col col-md-4 fontsize">
-                    Facility
-                </div>
-                <div class="col-md-5">
-                    <div class="form-group insert-form">
-                        <select name="ID_Facility" class="form-control">
-                            <?php foreach ($facilities as $fac): ?>
-                                <option value="<?php echo $fac['ID_facility']; ?>"><?php echo $fac['Name']; ?></option>
-                            <?php endforeach; ?>
-                        </select>
-
-
+                <div class="col-md-5 input-group">
+                    <select name="ID_Facility" class="form-control">
+                        <?php foreach ($facilities as $fac): ?>
+                        <option value="<?php echo $fac['ID_facility']; ?>"><?php echo $fac['Name']; ?></option>
+                        <?php endforeach; ?>
+                    </select>
                         <!--<input type="text" class="form-control" name="ID_Facility" placeholder="Add ID Facility number">-->
-                    </div>
                 </div>
+               
             </div>
             
-        </div> 
-        <div class="col-md-4">
+        </div>
+        <div class="col-md-3">
             <div class="form-group">
-                <label>Upload Image</label>
+                <p style="font-size: 20px; font-family: sans-serif; font-weight: Bold">Upload Image</p>
                 <input type="file" name="editImage" accept="image/*" onchange="loadFile(event)" size="20"><br>
                 <img  id="output" width="300px" hight="400px">
                 <script>
@@ -123,12 +117,14 @@
             </div>
         </div>
     </div>
-    <div class="row"> 
-        <div class="col col-md-4" style="text-align: center;">
-            <button type="submit"  class="btn btn-success btn-lg">Edit Residents</button>
-        </div> 
-        <div class="col col-md-4"></div>
-    </div>
+    <div class="row insert-row"> 
+            <div class="col col-md-4"></div>
+            <div class="col col-md-2">
+            <button type="submit"  class="btn btn-default btn-lg btn-block ">Edit Resident</button>
+           </div> 
+       
+            
+        </div>
 </div>
 </form>
 <!--Javascript libraries--> 
