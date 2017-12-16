221a9a5ae06f18fc972abf0f2dfb992e10cdf919
den-pauly <trex.ruben@hotmail.com>
2017-12-16 15:16:17 +0100

    recommit

--- application/views/pages_generalised/footer.php
+++ application/views/pages_generalised/footer.php
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-		
-
 		<div class="container">
 			<center><p style="font-size:15px;">Copyright: HCI/webapps project-team 2 &copy; 2017</p></center>
 		</div>
     </body>
 </html>
-=======
-		
-
-		<div class="container">
-			<center><p style="font-size:15px;">Copyright: HCI/webapps project-team 2 &copy; 2017</p></center>
-		</div>
-    </body>
-</html>
->>>>>>> 0fd2dfeed83f682f5ecbdaccdc1b79549f04a072
