b2c8ee8bd3eea90a94b934e69e0dfc601d135c4d
Lore <lore.donne@student.kuleuven.be>
2018-01-23 20:03:28 +0100

    Fixed little detail

--- application/views/pages_caregiver/viewResident.php
+++ application/views/pages_caregiver/viewResident.php
@@ -25,30 +25,7 @@
             <label>{Facility}:</label> <br> <label class="lab">&emsp;&emsp;&emsp;&emsp;<?php echo $fac_name['Name']; ?></label><br>
             <label>{MemberSince}:</label> <br> <label class="lab">&emsp;&emsp;&emsp;&emsp;<?php echo $residents['Member_Since']; ?></label>
 	</div>
-
-       <!-- <div id="forms" class="col-md-offset-0 col-sm-offset-1 col-sm-1" style="padding-left:0%">
-            <label>{LastName}:</label><br>
-            <label>{FirstName}:</label><br>
-            <label>{Gender}:</label><br>
-            <label>{Birthday}:</label>
-        </div>
-	<div id="forms" class=" col-md-offset-0 col-sm-offset-0 col-sm-1" style="padding-left:5%" data-step="2" data-intro="Here you can find Information of residents" data-position='right'>
-            <label class="lab pad2"> <?php echo $residents['LastName']; ?></label><br>
-            <label class="lab pad2"> <?php echo $residents['FirstName']; ?></label><br>
-            <label class="lab pad2"> <?php echo $residents['Sex']; ?></label><br>
-            <label class="lab pad2"> <?php echo $residents['Birthday']; ?></label>
-	</div>
-        
-        <div id="forms" class="col-md-offset-1 col-sm-offset-1 col-sm-1" style="padding-left:2.5%">
-            <label>{RoomNumber}:</label><br>
-            <label>{Facility}:</label><br>
-            <label>{MemberSince}:</label>
-        </div>
-        <div id="forms" class="col-md-offset-1 col-sm-offset-1 col-sm-2" style="padding-left:0%;" >
-            <label class="lab pad" > <?php echo $residents['RoomNumber']; ?></label><br>
-            <label class="lab pad" > <?php echo $fac_name['Name']; ?></label><br>
-            <label class="lab pad" > <?php echo $residents['Member_Since']; ?></label>
-	</div>-->
+    
     </div>
     <div class="row" style="padding-bottom: 4%; padding-top: 2%">
 		<?php if (htmlentities($this->session->userdata('permission')) >= '2'): ?>
@@ -56,7 +33,7 @@
         <div class="col-sm-offset-4 col-md-offset-4 col-md-2 col-sm-3" style="padding-left:0%;padding-right:0%">
             <button class="btn btn-lg btn-block button" value="EDIT" onclick="loadPage('CaregiverOperateResident', 'edit/<?php echo $residents['ID_Elder']; ?>')"/>{EDIT}</button>
         </div>
-        <div class="col-sm-offset-0 col-md-offset-1 col-sm-4 col-md-3" style="padding-left:2.5%; padding-right:5%">
+        <div class="col-sm-offset-0 col-md-offset-1 col-sm-4 col-md-3" style="padding-left:2.5%; padding-right:6%">
             
             <?php echo form_open('CaregiverOperateResident/delete/' . $residents['ID_Elder']); /* if we click it it goes to /post/delete/3 */ ?>    
 
