474c6556ac12b7ff7d417179094bc7683a02d678
Lore <lore.donne@student.kuleuven.be>
2018-01-25 11:53:58 +0100

    Update footer, added new category

--- application/views/pages_resident/categories.php
+++ application/views/pages_resident/categories.php
@@ -6,10 +6,16 @@
             <div class="col-sm-5">
                 <h2 class="par1">Grace-AGE</h2>
             </div>
-            <div class="col-sm-6"></div>
-            <div class="col-sm-1">
+            <div class="col-sm-offset-10">
+                <img id="log" class="topIcon" src="<?php echo base_url(); ?>/image/pictograms/logout.png"  value="Log_out" onclick="loadPage('ResidentLogin', 'view')"/>
+                <a id="logLink" class=" top" onclick="loadPage('ResidentLogin', 'view')">{Log_out}</a>              
+            </div>
+            <div class="col-sm-offset-11">
                 <a href="javascript:void(0);"  onclick="javascript:introJs().setOption('showProgress', true).start();">
                 <img src="<?php echo base_url(); ?>/image/pictograms/information.png" alt="" class="align-right"/>
+                </a>
+                <a href="javascript:void(0);"  onclick="javascript:introJs().setOption('showProgress', true).start();">
+                <a id="infoLink" class=" top" >Info</a>              
                 </a>
             </div>
         </div>
@@ -31,12 +37,12 @@
                     {Food}
                 </button>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%; padding-right:0.5%">
                 <button id = "newButton2"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=Comfort')">
                     {Comfort}
                 </button> 
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%;">
                 <button id = "newButton2"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=Activities')">
                     {Activities}
                 </button> 
@@ -44,22 +50,22 @@
         </div>
 
         <div class="row" >
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-right:0.5%">
                 <button id = "newButton3"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=PersonalRelationships')">
                     {Personal} <br/> {relations}
                 </button>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%; padding-right:0.5%">
                 <button id = "newButto3"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=DailyDecisions')">
                     {Daily} <br/> {decisions}
                 </button>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%; padding-right:0.5%">
                 <button id = "newButton"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=RespectByStaff')">
                     {Respect}
                 </button> 
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%;">
                 <button id = "newButton5"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=SafetyAndSecurity')">
                     {Safety}
                 </button> 
@@ -67,36 +73,34 @@
         </div>
 
         <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-right:0.5%">
                  <button id = "newButton"  type="button" class="btn btn-lg question button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=StaffResidentBonding')">
                     {Ga_terug}
 
                 </button> 
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%; padding-right:0.5%">
                 <button id = "newButton4"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=StaffResponsiveness')">
                     {responsiveness} </br> {the_caregivers}
                 </button> 
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3"style="padding-left:0.5%; padding-right:0.5%">
                  <button id = "newButton"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=StaffResidentBonding')">
                     {bonding}</br> {Staff}
 
                 </button> 
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3" style="padding-left:0.5%">
                  <button id = "newButton"  type="button" class="btn btn-lg style button btn-block" onclick="loadPage('ResidentQuestion','getQuestion?category=StaffResidentBonding')">
-                    {bonding}</br> {Staff}
+                     Informatie <br> woonzorgcentrum
 
                 </button> 
             </div>
         </div>  
         
-        <div class="row">
-		<div class="col-md-offset-10 col-sm-offset-9 col-sm-2">
-                   <a id = "link" type="button" class="btn btn-lg logout" onclick="loadPage('Welcome', 'Caregiver/overview')">{Log_out}</a>          
-                </div>     
-	</div>
+
+		    
+
 </div>
 
 
