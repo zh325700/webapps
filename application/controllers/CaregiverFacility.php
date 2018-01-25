9a1d4fc4df1031846fb1c86fe14e3c4fcdb14e75
Shuaigehan <zh325700@gmail.com>
2018-01-25 20:20:01 +0100

    fix ayano's page

--- application/controllers/CaregiverFacility.php
+++ application/controllers/CaregiverFacility.php
@@ -4,18 +4,18 @@ class CaregiverFacility extends CI_Controller {
 
     public function find() {
         $this->load->model('Language_model');
-        $data['text']=$this->Language_model->getData($this->session->userdata('language'),'findfac');
+        $data['text'] = $this->Language_model->getData($this->session->userdata('language'), 'findfac');
         $data['facility'] = $this->Addfacility_model->get_facility();
 
         $this->load->view('pages_generalised/header');
-        $this->parser->parse('pages_generalised/caregiver',$data['text']['header']);
+        $this->parser->parse('pages_generalised/caregiver', $data['text']['header']);
         $this->parser->parse('pages_caregiver/viewFacility', $data);
-        $this->parser->parse('pages_generalised/footer',$data['text']['footer']);
+        $this->parser->parse('pages_generalised/footer', $data['text']['footer']);
     }
-    
+
     public function view($ID_facility = NULL) {
         $this->load->model('Language_model');
-        $data=$this->Language_model->getData($this->session->userdata('language'),'addfac');
+        $data = $this->Language_model->getData($this->session->userdata('language'), 'addfac');
         $data['facility'] = $this->Addfacility_model->get_facility($ID_facility); // use post_model to get the data in the database
 
         if (empty($data['facility'])) {
@@ -23,25 +23,27 @@ class CaregiverFacility extends CI_Controller {
         }
         $data['$ID_facility'] = $data['facility']['ID_facility'];
         $this->load->view('pages_generalised/header');
-        $this->parser->view('pages_generalised/caregiver',$data['header']);
+        $this->parser->parse('pages_generalised/caregiver', $data['header']);
         $this->parser->parse('pages_caregiver/viewFacility', $data);
-        $this->parse->view('pages_generalised/footer',$data['footer']);
+        $this->parse->parse('pages_generalised/footer', $data['footer']);
     }
-       
+    
+
+
     public function addfacility() {
         //$data['title'] = 'Add Facility';
         $this->load->model('Language_model');
-        $data=$this->Language_model->getData($this->session->userdata('language'),'addfac');
+        $data = $this->Language_model->getData($this->session->userdata('language'), 'addfac');
         $data['title'] = 'Add Facility';
         $this->form_validation->set_rules('Name', 'Name', 'required');
         $this->form_validation->set_rules('City', 'City', 'required');
         if ($this->form_validation->run() === FALSE) {
             $this->load->view('pages_generalised/header');
-            $this->parser->parse('pages_generalised/caregiver',$data['header']);
-            $this->parser->parse('pages_caregiver/addFacility',$data);
-            $this->parser->parse('pages_generalised/footer',$data['footer']);
+            $this->parser->parse('pages_generalised/caregiver', $data['header']);
+            $this->parser->parse('pages_caregiver/addFacility', $data);
+            $this->parser->parse('pages_generalised/footer', $data['footer']);
         } else {
-                       
+
             $this->Addfacility_model->create_facility();
             redirect('addfacility_control/find');
         }
@@ -53,37 +55,37 @@ class CaregiverFacility extends CI_Controller {
     }
 
     public function edit($ID_facility = 1) {
-        $data['facility'] = $this->Addfacility_model->get_facility($ID_facility); // use post_model to get the data in the database
         $this->load->model('Language_model');
-        $data=$this->Language_model->getData($this->session->userdata('language'),'addfac');
+        $data = $this->Language_model->getData($this->session->userdata('language'), 'addfac');
+        $data['facility'] = $this->Addfacility_model->get_facility($ID_facility); // use post_model to get the data in the database
+
         if (empty($data['facility'])) {
             show_404();
         }
-            $data['title'] = 'Edit Facility';
-            $this->load->view('pages_generalised/header');
-            $this->parser->parse('pages_generalised/caregiver',$data['header']);
-            $this->parser->parse('pages_caregiver/editFacility', $data);
-            $this->parser->parse('pages_generalised/footer',$data['footer']);
+        $data['title'] = 'Edit Facility';
+        $this->load->view('pages_generalised/header');
+        $this->parser->parse('pages_generalised/caregiver', $data['header']);
+        $this->parser->parse('pages_caregiver/editFacility', $data);
+        $this->parser->parse('pages_generalised/footer', $data['footer']);
     }
+   
 
     public function update($ID_facility) {
+        $this->load->model('Language_model');
+        $data = $this->Language_model->getData($this->session->userdata('language'), 'addfac');
         $data['facility'] = $this->Addfacility_model->get_facility($ID_facility);
-        
         $this->form_validation->set_rules('Name', 'Name', 'required');
         $this->form_validation->set_rules('City', 'City', 'required');
         if ($this->form_validation->run() === FALSE) {
             $this->load->view('pages_generalised/header');
-            $this->load->view('pages_generalised/caregiver');
-            $this->load->view('pages_caregiver/editFacility',$data);
-            $this->load->view('pages_generalised/footer');
+            $this->load->view('pages_generalised/caregiver', $data['header']);
+            $this->load->view('pages_caregiver/editFacility', $data);
+            $this->load->view('pages_generalised/footer', $data['footer']);
         } else {
-                       
+
             $this->Addfacility_model->update_facility();
             redirect('CaregiverFacility/find'); //redirects
-           
         }
     }
-        
- }
 
-
+}
--- application/views/pages_caregiver/viewFacility.php
+++ application/views/pages_caregiver/viewFacility.php
@@ -1,44 +1,42 @@
 <?php if (htmlentities($this->session->userdata('permission')) >= '2' && htmlentities($this->session->userdata('allow_Caregiver')) == 'allow'): ?>
 
- <div class="container-fluid">
-    <div id="blue" class="row">
-         
-         <div class="col-sm-offset-0" style="padding-left:2.5%">
-            <h2>
-               <?php echo($text["Overview_Facility"]);?>
-            </h2>
-        </div>
-    </div> 
-    
-    <div class="row"> 
-        
-        <form method="post">         
-          <div id="forms" class="col-sm-offset-0">  
-                        
-			<h3><b>{facility}{Name}{/facility}</b></h3>
-			<label class="facility"><?php echo($text["City"]); ?> {facility}{City}{/facility}  </label><br>
-			<label class="facility"><?php echo($text["Postcode"]); ?> {facility}{Postcode}{/facility}</label><br> 
-			<label class="facility"><?php echo($text["Street"]); ?> {facility}{Street}{/facility} </label><br>
-			<label class="facility"><?php echo($text["Number"]); ?> {facility}{Number}{/facility} </label><br>
-            <?php if (htmlentities($this->session->userdata('permission')) >= '3'): ?>
-            
-            <div class="col-sm-offset-0 col-md-5 col-sm-5" style="padding-bottom:6%; padding-top: 2%;padding-left:0%; padding-right:2.5%">
-                <button class="btn btn-lg btn-block button" onclick="loadPage('CaregiverFacility', '/edit/1')"/> <?php echo($text["EDIT"]); ?></button>
-           </div> 
+    <div class="container-fluid ">
+        <div id="blue" class="row">
 
-            <div class="col-md-offset-2 col-sm-offset-2 col-md-5 col-sm-5" style="padding-bottom:6%; padding-top: 2%; padding-right:2.5%;padding-left:0%">
-                <button id="delete" class="btn btn-lg btn-block button" onclick="loadPage('CaregiverFacility','/delete/1')"> <?php echo($text["DELETE"]); ?></button>
-           </div>                                    
-            <?php endif; ?>
+            <div class="col-sm-offset-0" style="padding-left:2.5%">
+                <h2>
+                    <?php echo($text["Overview_Facility"]); ?>
+                </h2>
             </div>
-        </form>
-    </div>
-</div>
+        </div> 
 
+        <div class="row"> 
+
+                    
+                <div id="forms" class="col-sm-offset-0">  
+
+                    <h3><b>{facility}{Name}{/facility}</b></h3>
+                    <label class="facility"><?php echo($text["City"]); ?> {facility}{City}{/facility}  </label><br>
+                    <label class="facility"><?php echo($text["Postcode"]); ?> {facility}{Postcode}{/facility}</label><br> 
+                    <label class="facility"><?php echo($text["Street"]); ?> {facility}{Street}{/facility} </label><br>
+                    <label class="facility"><?php echo($text["Number"]); ?> {facility}{Number}{/facility} </label><br>
+                    <?php if (htmlentities($this->session->userdata('permission')) >= '3'): ?>
+                        <div class="col-sm-offset-0 col-md-5 col-sm-5" style="padding-bottom:6%; padding-top: 2%;padding-left:0%; padding-right:2.5%">
+                            <button class="button btn btn-lg btn-block " onclick="loadPage('CaregiverFacility', '/edit/1')"/> <?php echo($text["EDIT"]); ?></button>
+                        </div> 
+
+                        <div class="col-md-offset-2 col-sm-offset-2 col-md-5 col-sm-5" style="padding-bottom:6%; padding-top: 2%; padding-right:2.5%;padding-left:0%">
+                            <button id="delete" class="btn btn-lg btn-block button" onclick="loadPage('CaregiverFacility', '/delete/1')"> <?php echo($text["DELETE"]); ?></button>
+                        </div>                                    
+                    <?php endif; ?>
+                </div>
+           
+        </div>
+    </div>
 
 <?php else: ?>
-<p>
-<br><br><br>
-<span style="text-align: center;" class="error">You are not logged in or you are not authorized to access this page.</span> Please <a href="<?php echo base_url(); ?>">login</a> with the proper account.
-<br><br><br>
-<?php endif; ?>
+    <p>
+        <br><br><br>
+        <span style="text-align: center;" class="error">You are not logged in or you are not authorized to access this page.</span> Please <a href="<?php echo base_url(); ?>">login</a> with the proper account.
+        <br><br><br>
+    <?php endif; ?>
