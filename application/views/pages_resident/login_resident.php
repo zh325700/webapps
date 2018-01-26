882e9d0f6c494f3ace3cd57cf06ddbd05b6049a9
Lore <lore.donne@student.kuleuven.be>
2018-01-26 08:47:24 +0100

    Fixed mistake

--- application/views/pages_resident/login_resident.php
+++ application/views/pages_resident/login_resident.php
@@ -54,46 +54,6 @@
             </div>
         </div>
 
-        <div class="col-md-4 col-sm-4" style="padding-left: 2.5%" >
-            <button class="btn style btn-block" id="female_btn" onclick="location.href = '<?php echo base_url(); ?>index.php/ResidentLogin/loadBySex?sex=F'">
-                {Vrouw}
-            </button>
-        </div>
-        <div class="col-md-4 col-sm-4" style="padding-right: 2.5%">
-            <button class="btn style btn-block" id="male_btn" onclick="location.href = '<?php echo base_url(); ?>index.php/ResidentLogin/loadBySex?sex=M'">
-                {Man}
-            </button>
-        </div>
-    </div> 
-    <div class="row">
-        <div class="col-sm-12" style="padding-left: 2.5%">
-            <h1>
-                {Selecteer_foto}:
-            </h1>
-        </div>
-    </div>
-    <div class="row" data-step="2" data-intro="Choose your picture" data-position='top' style="padding-left:2.5%; padding-right:2.5%">
-        <div id="overviewResidents" class="col-md-12 border-style " style="overflow-y: scroll; height: 50%;">
-            <?php
-            foreach ($residents as $res) {
-                $fName = $res['FirstName'];
-                $lName = $res['LastName'];
-                $pic = $res['Picture'];
-                $id = $res['ID_Elder'];
-                ?>
-                <div class="col-sm-3 col-md-2" style="display: inline-block; margin-top: 2%; margin-bottom: 2%; margin-right: 4%; margin-left:4%">
-                    <img onclick="loadPage('ResidentLogin', 'verification?id=<?= $id ?>')"
-                         src="<?php echo base_url(); ?>image/photos/<?php echo $pic; ?>"
-                         alt="<?php echo $lName ?>" 
-                         style="width:200px ;height:200px;border:10px blue;">
-                    <div class="col-sm-6 figcaption"><?= $fName; ?> <?= $lName; ?> </div>
-                </div>
-                <?php
-            }
-            ?>
-        </div>
-    </div>
-</div>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <!-- Javascript libraries -->
 <?php
